cf1a20833692d1d444ec32c8debb4c9416c6f5cd
Vaibhav Saini <vaibhav.dev20@gmail.com>
2024-12-21 19:04:16 +0530

    Add ESLint configuration with updated rules and plugins

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import prettier from 'eslint-plugin-prettier'
+import jest from 'eslint-plugin-jest'
+
+export default [
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.node,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'prettier/prettier': 'warn',
+      'no-unused-vars': 'warn',
+      'no-console': 'error'
+    },
+  },
+  {
+    files: ['src/**/*.test.js'],
+    plugins: {
+      jest,
+    },
+    rules: {
+      ...jest.configs['flat/recommended'].rules,
+      'jest/prefer-expect-assertions': 'off',
+    },
+  },
+]
